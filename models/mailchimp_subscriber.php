62685e1be495b6022add45e2f1d082f01786f802
Ramon de Jesus <ramon.de.jesus@eduhub.nl>
2011-03-11 14:51:19 +0100

    first commit

---
+++ models/mailchimp_subscriber.php
@@ -0,0 +1,96 @@
+<?php
+class MailchimpSubscriber extends AppModel
+{
+	var $useDbConfig = 'mailchimp';
+    var $name = 'MailchimpSubscriber ';
+	var $useTable = false;
+
+	// $validate is really defined in the __construct constructor because of i18n issues
+	var $validate = array();
+
+	/**
+	 * Use $_schema to set any mailchimp fields that you want to use
+	 * @var <type>
+	 */
+	public $_schema = array(
+		'id' => array(
+			'type' => 'int',
+			'null' => true,
+			'key' => 'primary',
+			'length' => 11,
+		),
+		'emailaddress' => array(
+			'type' => 'string',
+			'null' => false,
+			'length' => 256
+		),
+		'FNAME' => array(
+			'type' => 'string',
+			'null' => true,
+			'key' => 'primary',
+			'length' => 128
+		),
+		'LNAME' => array(
+			'type' => 'string',
+			'null' => true,
+			'length' => 128
+		),
+		'GENDER' => array(
+			'type' => 'string',
+			'null' => true,
+			'length' => 32
+		),
+	);
+
+	/**
+	 * Override del method from model.php class, because it would block deleting when useTable = false and no records exists
+	 * @param <type> $id
+	 * @param <type> $cascade
+	 * @return <type>
+	 */
+	function del($id = null, $cascade = true) {
+
+		if (!empty($id)) {
+			$this->id = $id;
+		}
+		$id = $this->id;
+
+		if ( $this->beforeDelete($cascade)) {
+			$db =& ConnectionManager::getDataSource($this->useDbConfig);
+			if (!$this->Behaviors->trigger($this, 'beforeDelete', array($cascade), array('break' => true, 'breakOn' => false))) {
+				return false;
+			}
+			$this->_deleteDependent($id, $cascade);
+			$this->_deleteLinks($id);
+			$this->id = $id;
+
+			if (!empty($this->belongsTo)) {
+				$keys = $this->find('first', array('fields' => $this->__collectForeignKeys()));
+			}
+
+			if ($db->delete($this)) {
+				if (!empty($this->belongsTo)) {
+					$this->updateCounterCache($keys[$this->alias]);
+				}
+				$this->Behaviors->trigger($this, 'afterDelete');
+				$this->afterDelete();
+				$this->_clearCache();
+				$this->id = false;
+				$this->__exists = null;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function __construct($id = false, $table = null, $ds = null) {
+        parent::__construct($id, $table, $ds);
+        $this->validate = array(
+			'emailaddress' => array(
+		        'rule' => array('email'),
+		        'message' => __("Please enter a valid e-mail address", true)
+			)
+		);
+	}
+}
+?>
